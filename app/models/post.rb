73dbe60dec2294269df11260c2919b00b47bb6a2
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-29 22:01:16 -0700

    done with the assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
 class Post < ApplicationRecord
+
+    # all posts have a title
+    validates :title, presence: true
+
+    # post content is at least 250 characters long
+    validates :content, length: { minimum: 250 }
+
+    # post summary is a maximum of 250 characters
+    validates :summary, length: { maximum: 250 }
+
+    # post category is either Fiction or Non-Fiction
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
