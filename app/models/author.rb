a82b43acdc6becf15e96723326434d6e3216bb7e
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-29 22:00:52 -0700

    commit tree ad5037d397c9f1f9c08e924ae50ef4094871a944

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+
+    # all authors have a name; no 2 authors have the same name
+    validates :name, presence: true, uniqueness: true
+    # phone numbers are exactly ten digits
+    validates :phone_number, length: { is: 10 }
+
 end
